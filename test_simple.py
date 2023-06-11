643460b3076a7c99de7dd7e2fe832f2b49a968b8
VISerov <serov.qa@gmail.com>
2023-06-11 15:24:21 +0300

    Добавил тест создание выпуска. Исправил некоторые локаторы.

--- test_simple.py
+++ test_simple.py
@@ -1,7 +1,7 @@
 from selene import browser, be, have
 import time
 
-def test_for_authorization(size_window):
+def test_for_authorization(size_window): #Авторизация
     browser.open('https://redbasset.tech/')
     browser.element('[class="Button_button__341xg UserMenu_authButton__3bJH8 Button_button_outline__13DSB"]').click()
     browser.element('[name="email"][class="Input_input__3YQfM Input_input-common__1WU22"]').should(be.blank).type('yksyp@mailto.plus')
@@ -9,13 +9,13 @@ def test_for_authorization(size_window):
     browser.element('[class="Typography_root__3zCC6 Typography_root_body___AwBW UserMenu_userName__2uqUl"]').should(have.text('yksyp@mailto.plus'))
 
 
-def test_podcaster_cabinet(authorization):
+def test_podcaster_cabinet(authorization): #Переход в кабинет подкастера
     browser.element('[class="Typography_root__3zCC6 Typography_root_body___AwBW UserMenu_userName__2uqUl"]').click()
     browser.element('[class="UserMenu_dropLink__3JTzn"][href="/podcaster"]').click()
     # browser.element('[class ="Modal_close__2f-7E"]').click()
 
 
-def test_creating_author(podcaster_cabinet):
+def test_creating_author(podcaster_cabinet): #Создание автора
     browser.element('[class="Button_podcasterButton__115KA Button_button_secondary__2R4a7"][type="button"]').click()
     input_field = browser.element('input[type=file]')
     input_field.send_keys('C:/Users/hello/PycharmProjects/red_basset_pet/img/Mona_Lisa1.jpg')
@@ -34,8 +34,8 @@ def test_creating_author(podcaster_cabinet):
     browser.element('[class="SidebarItem_summaryRoot__39dBz SidebarItem_summaryRootActive__1hXt4"]').should(have.text('AQA Тест 123'))
 
 
-def test_creating_podcast(podcaster_cabinet):
-    browser.element('[class="Podcasts_link__1DQ6S"][href="/podcaster/podcast/new?authorId=1037"]').click()
+def test_creating_podcast(podcaster_cabinet): #Создание подкаста
+    browser.element('[class="Button_podcasterButton__115KA PodcasterMenu_button__2F0gP Button_button_primary__2JSyq"]').click()
     input_field = browser.element('input[type="file"]')
     input_field.send_keys('C:/Users/hello/PycharmProjects/red_basset_pet/img/QA.jpeg')
     browser.element('[name="mainInfo.name"]').should(be.blank).type('It\'s not me!')
@@ -54,14 +54,20 @@ def test_creating_podcast(podcaster_cabinet):
     browser.element('[name="additionalInfo.podcastUrl"]').should(be.blank).type('https://slozhna.redbasset.tech/')
     browser.element('[placeholder="Введите ссылку Vkontakte"]').should(be.blank).type('https://m.vk.com/lastsum')
     browser.element('[class="Button_podcasterButton__115KA Button_button_primary__2JSyq"]').click()
-    time.sleep(20)
 
 
-def test_creating_episode(podcaster_cabinet):
-    browser.element('[href="/podcaster/podcast/1234"]').click()
-    browser.element('[class="Episodes_link__3xhrC"][href="/podcaster/episode/new?podcastId=1234"]').click()
-    time.sleep(5)
+def test_creating_episode(podcaster_cabinet): #Создание выпуска
+    browser.element('[class="ListItem_item__jXXyv ListItem_link__3O1lO PodcastItem_card__3H_V1"]').click()
+    browser.element('[class="Button_podcasterButton__115KA PodcastMenu_button__1R8-i Button_button_primary__2JSyq"]').click()
     input_field = browser.element('[type="file"][class="InputAudioFile_nativeInput__2Y5Sp"]')
     input_field.send_keys('C:/Users/hello/PycharmProjects/red_basset_pet/audio/Как начинающему разработчику найти первую работу в IT (в гостях Влад Соколенко).mp3')
     browser.element('[type="text"][name="episodeInfo.name"]').should(be.blank).type('Как начинающему разработчику найти первую работу')
-    browser.element('[class="public-DraftEditorPlaceholder-inner"][id="placeholder-28r31"]').should(be.blank).type('Раз')
+    browser.element('//*[@id="__next"]/div[1]/div/div/main/div[2]/div/form/div[2]/div[1]/div[3]/div/div/div[3]/div[2]/div').type('Разбираемся, как можно получить свою первую работу в IT '
+                                                                                                                        'в качестве junior фронтенд-разработчика вместе с Алексеем Авдеевым, CTO в Mish,'
+                                                                                                                        ' и Владиславом Соколенко, разработчиком в Mish')
+    browser.element('//*[@id="__next"]/div[1]/div/div/main/div[2]/div/form/div[1]/ul/li[2]/button').click()
+    browser.element('[name="episodeSettings.seasonNumber"]').should(be.blank).type('1')
+    browser.element('[name="episodeSettings.episodeNumber"]').should(be.blank).type('1')
+    browser.element('//*[@id="__next"]/div[1]/div/div/main/div[2]/div/form/div[1]/ul/li[3]/button').click()
+    browser.element('//*[@id="__next"]/div[1]/div/div/main/div[2]/div/div/div/div/button[2]').click()
+    time.sleep(10)
