57133dc7c9942ff563b1bc04c073e4f3729ffbb4
Vladimir Serov <serov.qa@gmail.com>
2023-08-01 10:32:09 +0300

    Allure report1

--- tests/test_plan.py
+++ tests/test_plan.py
@@ -1,5 +1,14 @@
 from selene import browser
+import allure
+from allure_commons.types import Severity
 
 
-def test_issue_name_for_github_repo(browser_management):
-    browser.open('https://github.com/')
+@allure.tag('web')
+@allure.severity(Severity.BLOCKER)
+@allure.label('owner', 'nsbelova')
+@allure.feature('Issues names')
+@allure.story('Issues in public repository can be found')
+@allure.link('https://github.com', name='Testing')
+def test_issue_name_for_github_repo_dynamic_steps():
+    with allure.step('Open main page'):
+        browser.open('https://github.com/')
